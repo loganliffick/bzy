9c92a89bea78a51a323f0d690a79ca88e58871b5
Logan Liffick <ltliffick@gmail.com>
2023-02-11 19:41:42 -0500

    brand updates

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,12 +2,11 @@ import '@/styles/globals.scss';
 import Head from 'next/head';
 
 // fonts
-import { Lato, Newsreader } from '@next/font/google';
-const lato = Lato({ subsets: ['latin'], weight: ['400'] });
-const newsreader = Newsreader({
+import { Nunito, Pacifico } from '@next/font/google';
+const nunito = Nunito({ subsets: ['latin'] });
+const pacifico = Pacifico({
   subsets: ['latin'],
   weight: ['400'],
-  style: ['italic'],
 });
 
 const App = ({ Component, pageProps }) => {
@@ -20,24 +19,32 @@ const App = ({ Component, pageProps }) => {
         <link rel="icon" href="/favicon.svg" />
         <link rel="shortcut icon" href="/favicon.svg" />
 
+        <title>Bzy • Your new business card</title>
         <meta
-          property="og:image"
-          content="https://www.loganliffick.com/og.jpg"
+          name="description"
+          content="The digital business card platform focused on building community. Whether you're an individual or hosting an event, Bzy is the answer."
         />
+        <meta property="og:title" content="Bzy • Your new business card" />
+        <meta
+          property="og:description"
+          content="The digital business card platform focused on building community. Whether you're an individual or hosting an event, Bzy is the answer."
+        />
+
+        <meta property="og:image" content="https://www.bzy.so/og.jpg" />
         <meta property="og:type" content="website" />
-        <meta property="og:site_name" content="Logan Liffick" />
-        <meta property="og:url" content="https://loganliffick.com" />
-        <meta name="twitter:image:alt" content="© logan liffick" />
+        <meta property="og:site_name" content="Bzy" />
+        <meta property="og:url" content="https://bzy.so" />
+        <meta name="twitter:image:alt" content="Bzy" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@logan_liffick" />
-        <meta name="twitter:creator" content="@logan_liffick" />
+        <meta name="twitter:site" content="@letsgetbzy" />
+        <meta name="twitter:creator" content="@letsgetbzy" />
         <meta name="robots" content="index,follow" />
         <meta name="googlebot" content="index,follow" />
       </Head>
       <style jsx global>{`
         :root {
-          --lato: ${lato.style.fontFamily};
-          --newsreader: ${newsreader.style.fontFamily};
+          --nunito: ${nunito.style.fontFamily};
+          --pacifico: ${pacifico.style.fontFamily};
         }
       `}</style>
       <Component {...pageProps} />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 const Index = () => {
   return (
     <main>
-      <h1>
-        The new <span>business card</span>
+      <h1 className="title_large">
+        Your <span>new</span> business card
       </h1>
       <h2>test</h2>
     </main>
